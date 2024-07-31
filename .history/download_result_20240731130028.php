fc1b9f6b03af895f8157d28355a76e21dcb78536
walterjnr1 <newleastpaysolution@gmail.com>
2024-07-31 13:00:37 +0100

    fix download code

---
+++ .history/download_result_20240731130028.php
@@ -0,0 +1,17 @@
+<?php
+$filename = 'download/RESULT_CALCULATOR.xlsx';
+$basename = basename($filename);
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="'.$basename.'"');
+header('Content-Transfer-Encoding: binary');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+// header('Content-Length: '.$size);
+ob_clean();
+flush();
+readfile($filename);
+exit;
+		
+?>
